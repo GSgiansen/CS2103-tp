babcb0db6f09dc3f590013114a0932e9096e8563
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2023-11-10 23:19:38 +0800

    Fix check style

--- src/test/java/seedu/address/ui/ImportWindowTest.java
+++ src/test/java/seedu/address/ui/ImportWindowTest.java
@@ -1,18 +1,14 @@
 package seedu.address.ui;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.Start;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 
 @ExtendWith(ApplicationExtension.class)
